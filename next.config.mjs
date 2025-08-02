590caf95dd94770ee5f67f9e1bf0905c0e0c9911
Mathu-Mathi-M <mathu9147@gmail.com>
2025-08-02 09:59:06 +0530

    password reset

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,34 @@ const nextConfig = {
   output: 'standalone',
   experimental: {
     serverActions: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/auth/reset-password/confirm',
+        has: [
+          {
+            type: 'query',
+            key: 'token',
+          },
+        ],
+        permanent: false,
+        destination: '/auth/reset-password/confirm/:token',
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date' },
+        ],
+      },
+    ];
   }
 }
 
